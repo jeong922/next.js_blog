62188d66b3d49a88563487ab1561fe9a80aa8638
jeong922 <hwajeong922@naver.com>
2023-03-26 19:41:54 +0900

    PostCard 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@ import { Inter } from 'next/font/google';
 import styles from './page.module.css';
 import Profile from '@/components/Profile';
 import FeaturedPosts from '@/components/FeaturedPosts';
+import CarouselPosts from '@/components/CarouselPosts';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function HomePage() {
   return (
     <section className='py-4'>
       <Profile />
       <FeaturedPosts />
+      <CarouselPosts />
     </section>
   );
 }
---
+++ src/components/CarouselPosts.tsx
@@ -0,0 +1,3 @@
+export default function CarouselPosts() {
+  return <div></div>;
+}
