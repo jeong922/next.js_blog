9135cb10cbf1fa50a05882e3e66096d218092ec6
jeong922 <hwajeong922@naver.com>
2023-06-06 21:39:58 +0900

    header 디자인 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,34 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+  const pathname = usePathname();
   return (
     <header className='fixed z-30 flex flex-col items-center justify-between w-full py-5 bg-white border-b px-7 sm:flex-row max-w-screen-2xl'>
       <Link className='flex items-center text-2xl font-bold' href='/'>
         <h1>{"Jeong's Blog"}</h1>
       </Link>
       <nav className='flex gap-4 mt-4 text-sm uppercase sm:mt-0'>
-        <Link href='/'>Home</Link>
-        <Link href='/posts'>Posts</Link>
-        <Link href='/contact'>Contact</Link>
+        <Link
+          href='/'
+          className={`${pathname === '/' && 'text-blue-900 font-bold'}`}
+        >
+          Home
+        </Link>
+        <Link
+          href='/posts'
+          className={`${pathname === '/posts' && 'text-blue-900 font-bold'}`}
+        >
+          Posts
+        </Link>
+        <Link
+          href='/contact'
+          className={`${pathname === '/contact' && 'text-blue-900 font-bold'}`}
+        >
+          Contact
+        </Link>
       </nav>
     </header>
   );
